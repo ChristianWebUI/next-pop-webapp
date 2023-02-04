e2600912fab7e3317375f214a9d7e4ec84064c03
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 14:18:31 -0500

    feature: add new storage functionality to state and reducers

--- app/context/StoreContext.js
+++ app/context/StoreContext.js
@@ -1,17 +1,27 @@
 /* eslint-disable indent */
+import {
+  getDataFromLocalStorage,
+  STORAGE_KEYS,
+  storeData
+} from '@/utils/storage'
 import { createContext, useContext } from 'react'
 
 export const StoreContext = createContext()
 
-export const initialState = {
-  cart: { cartItems: [] }
+export const initialState = getInitialState()
+
+function getInitialState() {
+  if (getDataFromLocalStorage(STORAGE_KEYS.cart)) {
+    return getDataFromLocalStorage(STORAGE_KEYS.cart)
+  }
+  return { cart: { cartItems: [] } }
 }
 
 export function reducer(state, action) {
   switch (action.type) {
     case 'CART_ADD_ITEM': {
       const newItem = action.payload
-      const existItem = state.cart.cartItems.find(
+      const existItem = state.cart.cartItems?.find(
         (item) => item.slug === newItem.slug
       )
       const cartItems = existItem
@@ -19,11 +29,17 @@ export function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem]
+      storeData(STORAGE_KEYS.cart, {
+        ...state,
+        cart: { ...state.cart, cartItems }
+      })
       return { ...state, cart: { ...state.cart, cartItems } }
     }
 
-    default:
+    default: {
+      storeData(STORAGE_KEYS.cart, state)
       return state
+    }
   }
 }
 
