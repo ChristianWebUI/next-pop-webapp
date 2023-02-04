5139c47160d3d05b45846778755bdc87ec4e23ec
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 14:47:31 -0500

    fix: remove unnecessary comments

--- app/product/[slug]/head.jsx
+++ app/product/[slug]/head.jsx
@@ -1,7 +1,5 @@
 import data from '@/utils/data'
 
-/** We can use this file for changing head for each route */
-
 export default function Head({ params }) {
   const { slug } = params
   const product = data.products.find((prod) => prod.slug === slug)
