f7cbb5aabcef5143614b7c232ef8536c8c469e90
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 02:23:45 -0500

    feature: extract cart navigation link to a new client component

---
+++ app/components/CartNavigationLink.jsx
@@ -0,0 +1,21 @@
+'use client'
+import { useStoreContext } from '@/context/StoreContext'
+import Link from 'next/link'
+
+export default function CartNavigationLink({ id, route, label }) {
+  const { state } = useStoreContext()
+  const { cart } = state
+  return (
+    <Link key={id} href={route} className="p-2">
+      {label}
+      {cart.cartItems?.length > 0 && (
+        <span className="ml-1 rounded-full bg-red-600 py-1 px-2 text-xs font-bold text-white">
+          {cart.cartItems.reduce(
+            (totalItems, cartItem) => totalItems + cartItem.quantity,
+            0
+          )}
+        </span>
+      )}
+    </Link>
+  )
+}
