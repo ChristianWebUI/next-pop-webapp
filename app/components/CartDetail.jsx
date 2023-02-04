ab4817d9b96e6b658196ffac1b30f6f7755bd5b8
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 16:55:10 -0500

    feature: add new cart detail as client componet

---
+++ app/components/CartDetail.jsx
@@ -0,0 +1,30 @@
+'use client'
+import CartItemsList from '@/components/CartItemsList'
+import { useStoreContext } from '@/context/StoreContext'
+import Link from 'next/link'
+
+export default function CartDetail() {
+  const { state, dispatch } = useStoreContext()
+  const {
+    cart: { cartItems }
+  } = state
+  const removeItemHandler = (item) => {
+    dispatch({ type: 'CART_REMOVE_ITEM', payload: item })
+  }
+
+  return (
+    <>
+      <h1 className="mb-4 text-xl">Shopping Cart</h1>
+      {cartItems.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go shopping</Link>
+        </div>
+      ) : (
+        <CartItemsList
+          cartItems={cartItems}
+          onClickHanlder={removeItemHandler}
+        />
+      )}
+    </>
+  )
+}
