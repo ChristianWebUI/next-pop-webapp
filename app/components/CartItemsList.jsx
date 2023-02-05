7148ccbc70c88cb133fa29ad111b4d34720df9ff
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 22:51:41 -0500

    feature: validate if user is logged in to redirect to shipping or login page

--- app/components/CartItemsList.jsx
+++ app/components/CartItemsList.jsx
@@ -73,7 +73,7 @@ export default function CartItemsList({
             <div className="pb-3 text-xl">{`Subtotal (${numberOfItemsToCheckout()}) : $${calculateSubTotal()}`}</div>
           </li>
           <li>
-            <Link href="/shipping">
+            <Link href="login?redirect=/shipping">
               <button className="primary-button w-full">Check Out</button>
             </Link>
           </li>
