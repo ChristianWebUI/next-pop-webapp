7850657ad6c61d458336f5978ce0effe8ba08f06
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-04 19:44:33 -0500

    feature: add state and effect to CartNavigation link component

--- app/components/CartNavigationLink.jsx
+++ app/components/CartNavigationLink.jsx
@@ -1,19 +1,26 @@
 'use client'
 import { useStoreContext } from '@/context/StoreContext'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function CartNavigationLink({ route, label }) {
   const { state } = useStoreContext()
   const { cart } = state
+  const [cartItemsCount, setCartItemsCount] = useState(0)
+  useEffect(() => {
+    setCartItemsCount(
+      cart.cartItems.reduce(
+        (totalItems, cartItem) => totalItems + cartItem.quantity,
+        0
+      )
+    )
+  }, [cart.cartItems])
   return (
     <Link href={route} className="p-2">
       {label}
-      {cart.cartItems?.length > 0 && (
+      {cartItemsCount > 0 && (
         <span className="ml-1 rounded-full bg-red-600 py-1 px-2 text-xs font-bold text-white">
-          {cart.cartItems.reduce(
-            (totalItems, cartItem) => totalItems + cartItem.quantity,
-            0
-          )}
+          {cartItemsCount}
         </span>
       )}
     </Link>
