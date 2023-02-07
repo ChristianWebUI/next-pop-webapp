9bf0cf05766e6f14c8476e5e07d6dc5c7ec38598
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-06 23:48:18 -0500

    feature: add session provider as wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,23 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import MainContent from '@/components/MainContent'
+import SessionProvider from '@/context/SessionContextProvider'
 import StoreProvider from '@/context/StoreContextProvider'
 import 'styles/globals.css'
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <head />
       <body>
         <div className="flex min-h-screen flex-col justify-between">
-          <StoreProvider>
-            <Header />
-            <MainContent>{children}</MainContent>
-            <Footer />
-          </StoreProvider>
+          <SessionProvider session={session}>
+            <StoreProvider>
+              <Header />
+              <MainContent>{children}</MainContent>
+              <Footer />
+            </StoreProvider>
+          </SessionProvider>
         </div>
       </body>
     </html>
