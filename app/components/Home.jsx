d6f252dc49bc4404cc451bfa793f4237645067a6
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-08 01:35:14 -0500

    experimental: add tests to use useQuery hook in Home component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,5 +1,23 @@
+'use client'
 import Products from '@/components/Products'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_MESSAGE = gql`
+  query countAllUsers {
+    countUsers
+  }
+`
 
 export default function Home() {
-  return <Products />
+  const { data, loading, error } = useQuery(GET_MESSAGE)
+  if (loading) return <>Loading...</>
+  if (error) {
+    return <>Error: {error.message} ⚔️</>
+  }
+  return (
+    <>
+      {`Total number of users are: ${data.countUsers}`}
+      <Products />
+    </>
+  )
 }
