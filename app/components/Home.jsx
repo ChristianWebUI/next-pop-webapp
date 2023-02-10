d3c0f80444f7204af5ee5fc3df4dc04eee61bb08
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-10 14:54:33 -0500

    fix: revert experimental changes mage to home component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,23 +1,5 @@
-'use client'
 import Products from '@/components/Products'
-import { gql, useQuery } from '@apollo/client'
-
-const GET_MESSAGE = gql`
-  query countAllUsers {
-    countUsers
-  }
-`
 
 export default function Home() {
-  const { data, loading, error } = useQuery(GET_MESSAGE)
-  if (loading) return <>Loading...</>
-  if (error) {
-    return <>Error: {error.message} ⚔️</>
-  }
-  return (
-    <>
-      {`Total number of users are: ${data.countUsers}`}
-      <Products />
-    </>
-  )
+  return <Products />
 }
