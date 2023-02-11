2a620a1b8459887032690ab03e6926f696a48937
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-10 21:30:46 -0500

    feature: add new logic to login navigation link in order to display submenu and sign out user

--- app/components/LoginNavigationLink.jsx
+++ app/components/LoginNavigationLink.jsx
@@ -1,20 +1,45 @@
 'use client'
-import { useSession } from 'next-auth/react'
+import DropdownLink from '@/components/DropdownLink'
+import { useStoreContext } from '@/context/StoreContext'
+import { clearCookie, STORAGE_KEYS } from '@/utils/storage'
+import { Menu } from '@headlessui/react'
+import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 const SESSION_STATUS = {
   loading: 'loading'
 }
 
-export default function LoginNavigationLink({ key, route, label }) {
+export default function LoginNavigationLink({ key, route, label, subMenu }) {
+  const { dispatch } = useStoreContext()
   const { status, data: session } = useSession()
   const { loading } = SESSION_STATUS
+  const handleSignOut = () => {
+    clearCookie(STORAGE_KEYS.cart)
+    dispatch({ type: 'CART_RESET' })
+    signOut({ callbackUrl: '/login' })
+  }
   return status === loading ? (
     'Loading'
   ) : session?.user ? (
-    session.user.name
+    <Menu key={key} as="div" className="relative inline-block">
+      <Menu.Button className="text-blue-600">{session.user.name}</Menu.Button>
+      <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white shadow-lg">
+        {subMenu.map((subItem) => (
+          <Menu.Item key={subItem.route}>
+            <DropdownLink
+              className="dropdown-link"
+              href={subItem.route}
+              onClick={subItem.route === '#' ? handleSignOut : undefined}
+            >
+              {subItem.label}
+            </DropdownLink>
+          </Menu.Item>
+        ))}
+      </Menu.Items>
+    </Menu>
   ) : (
-    <Link href={route} className="p-2">
+    <Link key={key} href={route} className="p-2">
       {label}
     </Link>
   )
