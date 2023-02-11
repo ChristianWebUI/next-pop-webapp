1c17b5ed2518d287be7bf8febe67a55eef8fc361
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-10 21:32:00 -0500

    feature: new util function to clear a specific a desired storage key in cookies

--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 export function objectToBase64(object) {
   const string = JSON.stringify(object)
   return Buffer.from(string).toString('base64')
@@ -6,6 +8,10 @@ export function objectToBase64(object) {
 export function base64ToObject(base64) {
   const string = Buffer.from(base64, 'base64').toString('utf-8')
   return JSON.parse(string)
+}
+
+export function clearCookie(key) {
+  Cookies.remove(key)
 }
 
 export const STORAGE_KEYS = {
