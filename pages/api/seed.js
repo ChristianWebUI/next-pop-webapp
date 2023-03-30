b9da9d2074ad39d6fc5579984c5c5354d5e6dcf6
Christian Torres <christian.torres2893@gmail.com>
2023-03-29 22:25:39 -0500

    feature: add a bunch of product to database

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,4 +1,5 @@
 import db from '@/mongodb/db'
+import Product from '@/mongodb/models/Product'
 import User from '@/mongodb/models/User'
 import data from '@/utils/data'
 
@@ -6,6 +7,8 @@ export const handler = async (req, res) => {
   await db.connect()
   await User.deleteMany()
   await User.insertMany(data.users)
+  await Product.deleteMany()
+  await Product.insertMany(data.products)
   await db.disconnect()
   res.send({ message: 'seeded successfully' })
 }
