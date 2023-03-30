f64d391b85fc41818597c3fbb083a720c3381dc3
Christian Torres <christian.torres2893@gmail.com>
2023-03-29 19:31:22 -0500

    feature: create a middleware to avoid unauthenticated users access to protected routes

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      const path = req.nextUrl.pathname
+
+      if (path.startsWith('/admin')) {
+        return token?.isAdmin
+      }
+
+      return token !== null
+    }
+  }
+})
+
+export const config = { matcher: ['/shipping', '/admin/:path*'] }
