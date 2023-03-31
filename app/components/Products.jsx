e6abdb5a4f7d9f4b1d3a5ae49850361336d2390a
Christian Torres <christian.torres2893@gmail.com>
2023-03-30 22:19:22 -0500

    feature: bring all products from database instead of use static data

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,13 +1,29 @@
+'use client'
 import ProductCard from '@/components/ProductCard'
-import data from '@/utils/data'
+import { GET_ALL_PRODUCTS } from '@/graphql/queries/getAllProducts'
+import { useQuery } from '@apollo/client'
+import { useMemo } from 'react'
+
+function useProductQuery(query, options = {}) {
+  const { loading, error, data } = useQuery(query, options)
+
+  const memoizedData = useMemo(() => data, [data])
+
+  return { data: memoizedData, loading, error }
+}
 
 export default function Products() {
-  const { products } = data
+  const { data, loading } = useProductQuery(GET_ALL_PRODUCTS)
+  const products = data?.getAllProducts
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-      {products.map((product) => (
-        <ProductCard {...product} key={product.slug} />
-      ))}
+      {loading ? (
+        <>Loading products...</>
+      ) : (
+        products.map((product) => (
+          <ProductCard {...product} key={product.slug} />
+        ))
+      )}
     </div>
   )
 }
