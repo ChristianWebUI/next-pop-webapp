72e4b8a1049f63d9d1374b13853726f0edc9d31e
Christian Torres <christian.torres2893@gmail.com>
2023-03-30 22:17:08 -0500

    feature: add env variables to next js configuration

--- next.config.js
+++ next.config.js
@@ -14,5 +14,10 @@ module.exports = {
         hostname: 'picsum.photos'
       }
     ]
+  },
+  env: {
+    APOLLO_CLIENT_URI: 'http://localhost:4000/graphql',
+    MONGODB_URI:
+      'mongodb+srv://christian-pop-db:Ndz5qsZl96rRiKOs@cluster0.iq5nbpo.mongodb.net/pop-stationery-database?retryWrites=true&w=majority'
   }
 }
