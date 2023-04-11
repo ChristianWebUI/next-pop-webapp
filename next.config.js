01d94191d6e852af0a55103bf72733b07961c3de
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-10 19:00:33 -0500

    feature: add mercadopago dependency for button and configure public key

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ module.exports = {
   env: {
     APOLLO_CLIENT_URI: 'http://localhost:4000/graphql',
     MONGODB_URI:
-      'mongodb+srv://christian-pop-db:Ndz5qsZl96rRiKOs@cluster0.iq5nbpo.mongodb.net/pop-stationery-database?retryWrites=true&w=majority'
+      'mongodb+srv://christian-pop-db:Ndz5qsZl96rRiKOs@cluster0.iq5nbpo.mongodb.net/pop-stationery-database?retryWrites=true&w=majority',
+    MERCADOPAGO_PUBLIC_KEY: 'TEST-b880659d-e34b-4ba7-9845-c1de28680582'
   }
 }
