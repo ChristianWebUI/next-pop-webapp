eb0d0abaafe7612a19b54139fcfe3d3833d43ec5
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-10 19:03:09 -0500

    feature: split constants into new constants for payment route

--- app/constants/payment.js
+++ app/constants/payment.js
@@ -1,5 +1,30 @@
-export const AVAILABLE_PAYMENT_METHODS = [
-  'Paypal',
-  'Credit Card',
-  'MercadoPago'
+export const PAYPAL = 'Paypal'
+export const MERCADOPAGO = 'MercadoPago'
+export const CREDIT_CARD = 'Credit Card'
+
+export const MERCADOPAGO_PUBLIC_KEY = process.env.MERCADOPAGO_PUBLIC_KEY
+
+export const mercadoPagoCustomization = {
+  texts: {
+    action: 'pay',
+    valueProp: 'security_details'
+  }
+}
+
+export const PAYMENT_METHODS = [
+  {
+    id: 'paypal',
+    icon: '',
+    value: PAYPAL
+  },
+  {
+    id: 'mercadopago',
+    icon: '',
+    value: MERCADOPAGO
+  },
+  {
+    id: 'creditcard',
+    icon: '',
+    value: CREDIT_CARD
+  }
 ]
