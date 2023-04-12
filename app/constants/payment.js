b2231556280879ffc822d2238a7454d1a00d6f32
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-12 17:33:47 -0500

    feature: new constants for payment

--- app/constants/payment.js
+++ app/constants/payment.js
@@ -4,6 +4,11 @@ export const CREDIT_CARD = 'Credit Card'
 
 export const MERCADOPAGO_PUBLIC_KEY = process.env.MERCADOPAGO_PUBLIC_KEY
 
+export const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID
+
+export const USD_CURRENCY_CODE = 'USD'
+export const COP_CURRENCY_CODE = 'COP'
+
 export const mercadoPagoCustomization = {
   texts: {
     action: 'pay',
