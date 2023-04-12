a12b765ec8c58b094d6a0fa15f43ebc46c382f77
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-12 17:35:00 -0500

    feature: add logic for order page

--- app/hooks/useOrderView.js
+++ app/hooks/useOrderView.js
@@ -1,22 +1,38 @@
 import {
   MERCADOPAGO,
   MERCADOPAGO_PUBLIC_KEY,
+  PAYPAL,
+  USD_CURRENCY_CODE,
   mercadoPagoCustomization
 } from '@/constants/payment'
+import { CAPTURE_PAYPAL_PAYMENT } from '@/graphql-mutations/capturePayPalPayment'
+import { CREATE_PAYPAL_ORDER } from '@/graphql-mutations/createPayPalOrder'
 import { NEW_PREFERENCE } from '@/graphql-mutations/newPreference'
 import { GET_ORDER_BY_ID } from '@/graphql-queries/getOrderById'
+import { GET_PAYPAL_CLIENT_ID } from '@/graphql-queries/getPaypalClientId'
 import { getErrorMessage } from '@/utils/error'
-import { useMutation, useQuery } from '@apollo/client'
+import { useLazyQuery, useMutation, useQuery } from '@apollo/client'
 import { Wallet, initMercadoPago } from '@mercadopago/sdk-react'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 import { useEffect, useState } from 'react'
 
 export default function useOrderView({ orderId }) {
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer()
   const [order, setOrder] = useState(null)
+  const [loadingPay, setLoadingPay] = useState(false)
   const [preferenceId, setPreferenceId] = useState(null)
+  const [createPayPalOrder] = useMutation(CREATE_PAYPAL_ORDER)
+  const [capturePayPalPayment] = useMutation(CAPTURE_PAYPAL_PAYMENT, {
+    onCompleted: async () => {
+      setLoadingPay(false)
+      await refetch()
+    }
+  })
   const [newPreference] = useMutation(NEW_PREFERENCE, {
     variables: { storeOrderId: orderId }
   })
-  const { loading, error, data } = useQuery(GET_ORDER_BY_ID, {
+  const [paypalClientId] = useLazyQuery(GET_PAYPAL_CLIENT_ID)
+  const { loading, error, data, refetch } = useQuery(GET_ORDER_BY_ID, {
     variables: { id: orderId },
     onCompleted: async (data) => {
       try {
@@ -28,6 +44,22 @@ export default function useOrderView({ orderId }) {
           const { data } = await newPreference()
           setPreferenceId(data?.newPreference.preferenceId)
         }
+        if (
+          data?.getOrderById?.paymentMethod === PAYPAL &&
+          !data?.getOrderById?.isPaid
+        ) {
+          const {
+            data: { paypalClientId: clientId }
+          } = await paypalClientId()
+          paypalDispatch({
+            type: 'resetOptions',
+            value: {
+              'client-id': clientId,
+              currency: USD_CURRENCY_CODE
+            }
+          })
+          paypalDispatch({ type: 'setLoadingStatus', value: 'pending' })
+        }
       } catch (error) {
         console.log(error)
       }
@@ -35,21 +67,82 @@ export default function useOrderView({ orderId }) {
   })
 
   const renderPaymentButton = () => {
-    if (!preferenceId) {
-      return <div>Cargando boton de compra...</div>
-    } else {
-      return (
-        <Wallet
-          initialization={{ preferenceId }}
-          customization={mercadoPagoCustomization}
-        />
+    if (order && order.paymentMethod === MERCADOPAGO) {
+      if (!preferenceId) {
+        return <div>Cargando boton de compra...</div>
+      } else {
+        return (
+          <div className="w-full">
+            <Wallet
+              initialization={{ preferenceId }}
+              customization={mercadoPagoCustomization}
+            />
+          </div>
+        )
+      }
+    } else if (order && order.paymentMethod === PAYPAL) {
+      return isPending ? (
+        <div>Cargando boton de compra...</div>
+      ) : (
+        <>
+          <div className="w-full">
+            <PayPalButtons
+              createOrder={createOrder}
+              onApprove={onApprove}
+              onCancel={onCancel}
+              onError={onError}
+            />
+          </div>
+          {loadingPay && <div>Loading Payment...</div>}
+        </>
       )
     }
   }
 
   useEffect(() => {
-    if (data) setOrder(data?.getOrderById)
+    if (data) {
+      setOrder(data?.getOrderById)
+    }
   }, [data])
+
+  const createOrder = async () => {
+    const { totalPrice } = order
+    const {
+      data: {
+        createPayPalOrder: { id: paypalOrderId }
+      }
+    } = await createPayPalOrder({
+      variables: {
+        orderData: {
+          currencyCode: USD_CURRENCY_CODE,
+          totalAmount: totalPrice
+        }
+      }
+    })
+    return paypalOrderId
+  }
+
+  const onApprove = async (data) => {
+    try {
+      setLoadingPay(true)
+      const { orderID } = data
+      await capturePayPalPayment({
+        variables: { paymentData: { paypalOrderId: orderID, orderId } }
+      })
+      alert('Order has been successfully paid!')
+    } catch (error) {
+      alert(getErrorMessage(error))
+    }
+  }
+
+  const onCancel = async (data) => {
+    const { orderID } = data
+    alert(`Order ${orderID} has been canceled from Paypal portal`)
+  }
+
+  const onError = (err) => {
+    alert(getErrorMessage(err))
+  }
 
   return {
     loading,
