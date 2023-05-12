ee7bcc67b183d4302483c7464920b27cc249ab77
chrisdev-ui <web.christian.dev@gmail.com>
2023-05-12 13:11:51 -0500

    feat: add rainbow loader

---
+++ app/components/RainbowLoader.jsx
@@ -0,0 +1,8 @@
+export default function RainbowLoader() {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <h1 className="rainbow-title">Loading</h1>
+      <div className="rainbow-marker-loader"></div>
+    </div>
+  )
+}
